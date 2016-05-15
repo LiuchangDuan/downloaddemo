f7150a568f2285c48e279df4ce9415828f81d8f4
LiuchangDuan <870121425@qq.com>
2016-05-15 17:54:00 +0800

    修改界面显示多个下载文件

---
+++ src/com/example/downloaddemo/FileListAdapter.java
@@ -0,0 +1,108 @@
+package com.example.downloaddemo;
+
+import java.util.List;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.entities.FileInfo;
+import com.example.services.DownloadService;
+
+/**
+ * 文件列表适配器
+ */
+public class FileListAdapter extends BaseAdapter {
+	
+	private Context mContext = null;
+	private List<FileInfo> mFileList = null;
+	
+	public FileListAdapter(Context mContext, List<FileInfo> mFileList) {
+		super();
+		this.mContext = mContext;
+		this.mFileList = mFileList;
+	}
+
+	/**
+	 * getCount方法用于返回需要在ListView上显示的总数
+	 * 要让文件集合中的文件显示在列表上，所以返回的是文件集合对象的size()方法
+	 * 返回文件个数
+	 */
+	@Override
+	public int getCount() {
+		return mFileList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return mFileList.get(position);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View view, ViewGroup viewGroup) {
+		ViewHolder holder = null;
+		if (view == null) {
+			//加载视图
+			view = LayoutInflater.from(mContext).inflate(R.layout.listitem, null);
+			//获得布局中的控件
+			holder = new ViewHolder();
+			holder.tvFileName = (TextView) view.findViewById(R.id.tvFileName);
+			holder.btStop = (Button) view.findViewById(R.id.btStop);
+			holder.btStart = (Button) view.findViewById(R.id.btStart);
+			holder.pbProgress = (ProgressBar) view.findViewById(R.id.pbProgress);
+			view.setTag(holder);
+		} else {
+			holder = (ViewHolder) view.getTag();
+		}
+		//设置视图中的控件
+		final FileInfo fileInfo = mFileList.get(position);
+		holder.tvFileName.setText(fileInfo.getFileName());
+		holder.pbProgress.setMax(100);
+		holder.pbProgress.setProgress(fileInfo.getFinished());
+		holder.btStart.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//通知Service开始下载
+				Intent intent = new Intent(mContext, DownloadService.class);
+				intent.setAction(DownloadService.ACTION_START);
+				intent.putExtra("fileInfo", fileInfo);
+				mContext.startService(intent);
+			}
+		});
+		holder.btStop.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//通知Service停止下载
+				Intent intent = new Intent(mContext, DownloadService.class);
+				intent.setAction(DownloadService.ACTION_STOP);
+				intent.putExtra("fileInfo", fileInfo);
+				mContext.startService(intent);
+			}
+		});
+		return view;
+	}
+	
+	/**
+	 * ViewHolder就是一个临时的储存器，把每次getView方法中每次返回的View缓存起来，可以下次再用。
+	 * 这样做的好处就是不必每次都到布局文件中来查找控件
+	 */
+	static class ViewHolder {
+		TextView tvFileName;
+		Button btStop, btStart;
+		ProgressBar pbProgress;
+	}
+
+}
--- src/com/example/downloaddemo/MainActivity.java
+++ src/com/example/downloaddemo/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.downloaddemo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -7,9 +10,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.example.entities.FileInfo;
 import com.example.services.DownloadService;
@@ -17,49 +18,70 @@ import com.example.services.DownloadService;
 
 public class MainActivity extends Activity {
 	
-	private TextView mTvFileName = null;
+	private ListView mLvFile = null;
+	private List<FileInfo> mFileList = null;
+	private FileListAdapter mAdapter = null;
 	
-	private ProgressBar mPbProgress = null;
-	
-	private Button mBtStop = null;
-	
-	private Button mBtStart = null;
+//	private TextView mTvFileName = null;
+//	
+//	private ProgressBar mPbProgress = null;
+//	
+//	private Button mBtStop = null;
+//	
+//	private Button mBtStart = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //初始化组件
-        mTvFileName = (TextView) findViewById(R.id.tvFileName);
-        mPbProgress = (ProgressBar) findViewById(R.id.pbProgress);
-        mBtStop = (Button) findViewById(R.id.btStop);
-        mBtStart = (Button) findViewById(R.id.btStart);
-        mPbProgress.setMax(100);
+        mLvFile = (ListView) findViewById(R.id.lvFile);
+        //创建文件集合
+        mFileList = new ArrayList<FileInfo>();
+//        mTvFileName = (TextView) findViewById(R.id.tvFileName);
+//        mPbProgress = (ProgressBar) findViewById(R.id.pbProgress);
+//        mBtStop = (Button) findViewById(R.id.btStop);
+//        mBtStart = (Button) findViewById(R.id.btStart);
+//        mPbProgress.setMax(100);
         //创建文件信息对象
-        final FileInfo fileInfo = new FileInfo(0, "http://www.imooc.com/mobile/imooc.apk", 
+        FileInfo fileInfo = new FileInfo(0, "http://www.imooc.com/mobile/imooc.apk", 
         		"imooc.apk", 0, 0);
+        FileInfo fileInfo1 = new FileInfo(1, "http://www.imooc.com/download/Activator.exe", 
+        		"Activator.exe", 0, 0);
+        FileInfo fileInfo2 = new FileInfo(2, "http://www.imooc.com/download/iTunes64Setup.exe", 
+        		"iTunes64Setup.exe", 0, 0);
+        FileInfo fileInfo3 = new FileInfo(3, "http://www.imooc.com/download/SkyGameInstall99602.exe", 
+        		"SkyGameInstall99602.exe", 0, 0);
+        mFileList.add(fileInfo);
+        mFileList.add(fileInfo1);
+        mFileList.add(fileInfo2);
+        mFileList.add(fileInfo3);
+        //创建适配器
+        mAdapter = new FileListAdapter(this, mFileList);
+        //设置ListView
+        mLvFile.setAdapter(mAdapter);
         //添加事件监听
-        mBtStart.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				//通过Intent传递参数给Service
-				Intent intent = new Intent(MainActivity.this, DownloadService.class);
-				intent.setAction(DownloadService.ACTION_START);
-				intent.putExtra("fileInfo", fileInfo);
-				startService(intent);
-			}
-		});
-        mBtStop.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(MainActivity.this, DownloadService.class);
-				intent.setAction(DownloadService.ACTION_STOP);
-				intent.putExtra("fileInfo", fileInfo);
-				startService(intent);
-			}
-		});
+//        mBtStart.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				//通过Intent传递参数给Service
+//				Intent intent = new Intent(MainActivity.this, DownloadService.class);
+//				intent.setAction(DownloadService.ACTION_START);
+//				intent.putExtra("fileInfo", fileInfo);
+//				startService(intent);
+//			}
+//		});
+//        mBtStop.setOnClickListener(new View.OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				Intent intent = new Intent(MainActivity.this, DownloadService.class);
+//				intent.setAction(DownloadService.ACTION_STOP);
+//				intent.putExtra("fileInfo", fileInfo);
+//				startService(intent);
+//			}
+//		});
         //注册广播接收器
         IntentFilter filter = new IntentFilter();
         filter.addAction(DownloadService.ACTION_UPDATE);
@@ -80,7 +102,7 @@ public class MainActivity extends Activity {
 		public void onReceive(Context arg0, Intent intent) {
 			if (DownloadService.ACTION_UPDATE.equals(intent.getAction())) {
 				int finished = intent.getIntExtra("finished", 0);
-				mPbProgress.setProgress(finished);
+//				mPbProgress.setProgress(finished);
 			}
 		}
     	

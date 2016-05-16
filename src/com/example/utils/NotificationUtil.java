44bb3fa75fa27c4cd2d760b4628f08a0bd4efa64
LiuchangDuan <870121425@qq.com>
2016-05-16 13:17:59 +0800

    添加通知

---
+++ src/com/example/utils/NotificationUtil.java
@@ -0,0 +1,81 @@
+package com.example.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.downloaddemo.MainActivity;
+import com.example.downloaddemo.R;
+import com.example.entities.FileInfo;
+import com.example.services.DownloadService;
+
+/**
+ * 通知工具类
+ * @author Administrator
+ *
+ */
+public class NotificationUtil {
+
+	private NotificationManager mNotificationManager = null;
+	private Map<Integer, Notification> mNotifications = null;
+	private Context mContext = null;
+	
+	public NotificationUtil(Context context) {
+		mContext = context;
+		//获得通知系统服务
+		mNotificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+		//创建通知的集合
+		mNotifications = new HashMap<Integer, Notification>();
+	}
+	
+	/**
+	 * 显示通知
+	 * @param fileInfo
+	 */
+	public void showNotification(FileInfo fileInfo) {
+		//判断通知是否已经显示了
+		if (!mNotifications.containsKey(fileInfo.getId())) {
+			//创建通知对象
+			Notification notification = new Notification();
+			//设置滚动文字
+			notification.tickerText = fileInfo.getFileName() + "开始下载";
+			//设置显示时间
+			notification.when = System.currentTimeMillis();
+			//设置图标
+			notification.icon = R.drawable.ic_launcher;
+			//设置通知特性
+			notification.flags = Notification.FLAG_AUTO_CANCEL;
+			//设置点击通知栏的操作
+			Intent intent = new Intent(mContext, MainActivity.class);
+			PendingIntent pintent = PendingIntent.getActivity(mContext, 0, intent, 0);
+			notification.contentIntent = pintent;
+			//创建RemoteViews对象
+			RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.notification);
+			//设置开始按钮操作
+			Intent intentStart = new Intent(mContext, DownloadService.class);
+			intentStart.setAction(DownloadService.ACTION_START);
+			intentStart.putExtra("fileInfo", fileInfo);
+			PendingIntent piStart = PendingIntent.getService(mContext, 0, intentStart, 0);
+			remoteViews.setOnClickPendingIntent(R.id.btStart, piStart);
+			//设置结束按钮操作
+			Intent intentStop = new Intent(mContext, DownloadService.class);
+			intentStop.setAction(DownloadService.ACTION_STOP);
+			intentStop.putExtra("fileInfo", fileInfo);
+			PendingIntent piStop = PendingIntent.getService(mContext, 0, intentStop, 0);
+			remoteViews.setOnClickPendingIntent(R.id.btStop, piStop);
+			//设置Notification的视图
+			notification.contentView = remoteViews;
+			//发出通知广播
+			mNotificationManager.notify(fileInfo.getId(), notification);
+			//把通知加到集合中
+			mNotifications.put(fileInfo.getId(), notification);
+		}
+	}
+	
+}
